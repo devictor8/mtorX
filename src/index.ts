961e3bf68b9ce2fe6a112fe0ba57354667855752
Keisy <k22.lizandra@gmail.com>
2026-06-22 21:12:13 -0300

    feat: refactor server initialization to use BackendRegistry and HealthChecker

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-import { LoadBalancer } from "./infra/loadBalancer";
-import { ServerPool } from "./infra/serverPool";
+import { BackendRegistry } from "./application/backendRegistry.js";
+import { envVariables } from "./config/envLoader.js";
+import { ControlPlaneServer } from "./infra/controlPlaneServer.js";
+import { DataPlaneServer } from "./infra/dataPlaneServer.js";
+import { HealthChecker } from "./infra/healthChecker.js";
 
-const serverPool = new ServerPool();
+const registry = new BackendRegistry(envVariables.NODE_TTL_MS);
+const healthChecker = new HealthChecker(
+  registry,
+  envVariables.HEALTH_CHECK_INTERVAL_MS,
+  envVariables.HEALTH_CHECK_TIMEOUT_MS,
+);
+const controlPlane = new ControlPlaneServer(
+  registry,
+  healthChecker,
+  envVariables.REGISTRY_PORT,
+  envVariables.REGISTRY_TOKEN,
+);
+const dataPlane = new DataPlaneServer(registry, envVariables.PORT);
 
-const servers = await serverPool.createServers(3);
-const loadBalancer = new LoadBalancer(servers);
-loadBalancer.start();
+healthChecker.start();
+controlPlane.start();
+dataPlane.start();
