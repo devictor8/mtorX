cd4063a2bce5e5c606bd956c6116e496621ebb11
Keisy <k22.lizandra@gmail.com>
2026-07-03 19:56:20 -0300

    refactor: remove unused properties from BackendTarget interface

--- src/domain/backendTarget.ts
+++ src/domain/backendTarget.ts
@@ -1,10 +1,4 @@
 export interface BackendTarget {
-  id: string;
   host: string;
   port: number;
-  healthPath: string;
-  isHealthy: boolean;
-  lastSeen: number;
-  lastChecked?: number;
-  lastError?: string;
 }
